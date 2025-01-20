b6ee0c2e7567910667e714d6156dbd38e12a2e16
Lugard Agu <107192340+lugard1@users.noreply.github.com>
2025-01-20 19:12:05 +0100

    Update the codebase to capture the exclusivity of 12 noon and 12 midnight

--- index.js
+++ index.js
@@ -7,14 +7,32 @@ function updateClock(){
   let h = new Date().getHours();
   let m = new Date().getMinutes();
   let s = new Date().getSeconds();
-  let ampm = "AM";
+  let ampm = h >= 12 ? "PM" : "AM";
 
-  if(h > 12){
-    h = h -12
-    ampm = "PM"
+  
+  if(h === 0){
+    h = 12 // midnight
+  } else if(h > 12){
+    h = h - 12
   }
 
-  h = h < 10 ? "0" + h : h;
+  // h = h < 10 ? "0" + h : h;
+  // m = m < 10 ? "0" + m : m;
+  // s = s < 10 ? "0" + s : s;
+
+  // or
+
+  if(h < 10) {
+    h = "0" + h
+  } 
+  
+  if(m < 10){
+    m = "0" + m
+  }
+
+  if(s < 10){
+    s = "0" + s
+  }
 
   hourEl.innerText = h
   minuteEl.innerText = m;
